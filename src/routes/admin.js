f487bc2bb88055ab438581e014b0a2228e14b91d
Sazin14 <mdabidhasanm111@gmail.com>
2024-12-28 04:15:01 +0600

    admin dashboard and top issue fetch by aggregate pipelining

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Issue = require("../models/Issue");
+const {logout} = require("../controllers/authincationC")
 
 router.get("/", checkLogin, async (req, res)=>{
-    
+    let admin = req.user;
+    res.render("adminDashboard", {admin});
 })
+
+
+router.post('/topIssues', async (req, res) => {
+  try {
+    const topIssues = await Issue.aggregate([
+      {
+        $addFields: {
+          upvoteCount: { $size: '$upVotes' } 
+        }
+      },
+      {
+        $sort: { upvoteCount: -1 }
+      },
+      {
+        $limit: 10 
+      }
+    ]);
+
+    res.json(topIssues); 
+  } catch (error) {
+    console.error('Error fetching top issues:', error);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
+
+
+router.get("/logout", logout);
